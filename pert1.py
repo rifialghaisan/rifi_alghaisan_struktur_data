41162dfc5474aeabca1a0ccb6c6052fa698b27fd
rifialghaisan <muhammadrifial270@gmail.com>
2026-04-22 12:30:02 +0800

    pert1
    saya ingin mengumpulkan tugas struktur data

---
+++ pert1.py
@@ -0,0 +1,13 @@
+nama =input("masukan nama anda :")
+
+if nama == "rifi":
+    print("selamat datang rifi")
+umur = int(input("masukan umur anda :"))
+
+if umur == 19:
+   print("umur anda masih remaja")
+elif umur == 15:
+    print("umur anda masih kecil")
+elif umur == 101:
+    print("umur anda sudah tua")
+print("program selesai")
