7933605e8ae6ef442f10b7fe8863b575d741856a
rifialghaisan <muhammadrifial270@gmail.com>
2026-04-22 12:32:09 +0800

    pert3
    saya ingin mengumpulkan tugas struktur data

---
+++ pert3.py
@@ -0,0 +1,8 @@
+angka = int(input("masukan angka : "))
+
+while angka < -7 :
+    print("harus positif")
+    angka = int(input("masukan angka: "))
+
+print("angka yang dimasukan adalah: ", angka)
+print("program selesai")
