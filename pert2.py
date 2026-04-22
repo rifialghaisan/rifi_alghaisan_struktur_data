e68c2254f182f15db60846c7c8d120dc9522ee75
rifialghaisan <muhammadrifial270@gmail.com>
2026-04-22 12:30:56 +0800

    pert2
    saya ingin mengumpulkan tugas struktur data

---
+++ pert2.py
@@ -0,0 +1,23 @@
+nama = input("masukan nama anda")
+
+if nama== "rifi" :
+    print("selamat datang rifi" \
+    "")   
+
+angka = [5]
+for i in angka : 
+    print("5 * 1", "=", "5")
+    print("5 * 2", "=", "10")
+    print("5 * 4", "=", "20")
+    print("5 * 5", "=", "25")
+    print("5 * 6", "=", "30")
+    print("5 * 7", "=", "35")
+    print("5 * 8", "=", "40")
+    print("5 * 9", "=", "45")
+    print("5 * 10", "=","50")
+    
+angka = range(1, 11)
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
+
+print("program selesai")
